39b9a5c74ff4c439c5bae9cd5e89d3c45e6ba598
dam <dam@10.100.100.19>
2021-02-23 13:26:54 +0100

    Segunda Version Dos lineas de código en Clase1

--- Refactorizar/src/refactorizar/Clase1.java
+++ Refactorizar/src/refactorizar/Clase1.java
@@ -15,7 +15,8 @@ public class Clase1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola Mundo");
+        System.out.println(2+5);
     }
     
 }
