db1048acf3143305806751d434adfaedc6e1d722
dam <dam@10.100.100.19>
2021-02-23 13:25:58 +0100

    Primera Version

--- Refactorizar/src/refactorizar/Clase1.java
+++ Refactorizar/src/refactorizar/Clase1.java
@@ -15,8 +15,7 @@ public class Clase1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
-        System.out.println(2+5);
+        // TODO code application logic here
     }
     
 }
